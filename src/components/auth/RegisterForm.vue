f4fe662ce90d3ba4fa008c075b15f8126670c123
Emmanuel Arante <57925823+earante@users.noreply.github.com>
2025-04-03 14:36:15 +0800

    fix: change the background and color of click here to login and register

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -9,7 +9,7 @@
     <v-text-field variant="outlined" label="Password" type="password"></v-text-field>
     <v-text-field variant="outlined" label="Confirm Password" type="password"></v-text-field>
 
-    <v-btn class="mt-2" type="submit" color="primary" prepend-icon="mdi-account-plus" block
+    <v-btn class="mt-2" type="submit" color="deep-orange" prepend-icon="mdi-account-plus" block
       >Register</v-btn
     >
   </v-form>
