5e116ae72aae7b459bb194d4e935780b5e003524
Emmanuel Arante <57925823+earante@users.noreply.github.com>
2025-04-21 17:47:09 +0800

    feat: added password view func

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,8 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+const isPasswordVisible = ref(false)
+</script>
+
 <template>
   <v-form fast-fail @submit.prevent>
-    <v-text-field variant="outlined" label="Email" type="email"></v-text-field>
+    <v-text-field
+      density="compact"
+      placeholder="Email address"
+      prepend-inner-icon="mdi-email-outline"
+      label="Email"
+      outlined
+      dense
+    ></v-text-field>
 
-    <v-text-field variant="outlined" label="Password" type="password"></v-text-field>
+    <v-text-field
+      :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+      :type="isPasswordVisible ? 'text' : 'password'"
+      density="compact"
+      placeholder="Enter your password"
+      prepend-inner-icon="mdi-lock-outline"
+      label="Password"
+      outlined
+      dense
+      @click:append-inner="isPasswordVisible = !isPasswordVisible"
+    ></v-text-field>
 
     <v-btn class="mt-2" color="deep-orange" type="submit" prepend-icon="mdi-login" block
       >Login</v-btn
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,13 +1,71 @@
+<script setup>
+import { ref } from 'vue'
+
+const isPasswordVisible = ref(false)
+const isConfirmPasswordVisible = ref(false)
+</script>
+
 <template>
   <v-form fast-fail @submit.prevent>
-    <v-text-field variant="outlined" label="Firstname" type="text"></v-text-field>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-text-field
+          v-model="firstname"
+          label="Firstname"
+          outlined
+          dense
+          :rules="nameRules"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-text-field
+          v-model="lastname"
+          label="Lastname"
+          outlined
+          dense
+          :rules="nameRules"
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
-    <v-text-field variant="outlined" label="Lastname" type="text"></v-text-field>
+    <v-text-field
+      density="compact"
+      placeholder="Email address"
+      outlined
+      dense
+      prepend-inner-icon="mdi-email-outline"
+      label="Email"
+      type="email"
+    ></v-text-field>
 
-    <v-text-field variant="outlined" label="Email" type="email"></v-text-field>
-
-    <v-text-field variant="outlined" label="Password" type="password"></v-text-field>
-    <v-text-field variant="outlined" label="Confirm Password" type="password"></v-text-field>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-text-field
+          :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="isPasswordVisible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline"
+          label="Password"
+          outlined
+          dense
+          @click:append-inner="isPasswordVisible = !isPasswordVisible"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-text-field
+          :append-inner-icon="isConfirmPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="isConfirmPasswordVisible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Confirm password"
+          prepend-inner-icon="mdi-lock-outline"
+          label="Confirm password"
+          outlined
+          dense
+          @click:append-inner="isConfirmPasswordVisible = !isConfirmPasswordVisible"
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
     <v-btn class="mt-2" type="submit" color="deep-orange" prepend-icon="mdi-account-plus" block
       >Register</v-btn
